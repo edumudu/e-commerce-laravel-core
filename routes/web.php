f03d3632460a8c6844827146202fbebd9e27d6e8
edumudu <eduardomudutiu@gmail.com>
2020-02-03 01:47:21 -0300

    create controllers

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,18 @@
 */
 
 Route::get('/', ['as' => 'site.home', 'uses' => 'Site\HomeController@execute']);
+Route::get('/contato', ['as' => 'site.contact', 'uses' => 'Site\ContactController@execute']);
+Route::get('/sobre', ['as' => 'site.about', 'uses' => 'Site\AboutController@execute']);
+Route::get('/produtos', ['as' => 'site.products', 'uses' => 'Site\ProductController@execute']);
+
+Route::get('/cart', ['as' => 'site.cart', 'uses' => 'Site\CartController@execute']);
+
+// Login
+Route::get('/login', ['as' => 'login', 'uses' => 'Login\LoginController@execute']);
+Route::post('/logar', ['as' => 'login.logar', 'uses' => 'Login\LoginController@login']);
+Route::get('/logout', ['as' => 'login.logout', 'uses' => 'Login\LoginController@logout']);
+
+// Disponiveis para qulquer um logado
+Route::group(['middleware' => 'auth'], function(){
+    Route::get('/whitelist', ['as' => 'site.whitelist', 'uses' => 'Site\UserController@whitelist']);
+});
